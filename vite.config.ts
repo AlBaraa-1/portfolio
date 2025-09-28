6ff4b8f78b46d3059d34e5cbfd36cd2888501b0b
AlBaraa-1 <666645@gmail.com>
2025-09-28 22:17:24 +0400

    Configure for dual deployment (Vercel and GitHub Pages)

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/portfolio/', // Replace 'portfolio' with your repository name
+  base: mode === 'github-pages' ? '/portfolio/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+}));
